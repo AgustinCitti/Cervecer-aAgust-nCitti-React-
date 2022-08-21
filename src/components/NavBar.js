5a359605e5b6c5e5bea6c2d2264c29151a9bc759
AgustinCitti <103970902+AgustinCitti@users.noreply.github.com>
2022-08-21 20:38:03 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+
+
+
+const NavBar=() => {
+    return(
+        <div>
+        <ul class="NavBar">
+  <li role="presentation" ><a class="NavBarItemBrand" href="#">CATELO</a></li>
+  <li role="presentation" ><a class="NavBarItem" href="#">Nosotros</a></li>
+  <li role="presentation" ><a class="NavBarItem" href="#">Contactanos</a></li>
+</ul></div>
+    );
+};
+export default NavBar;
