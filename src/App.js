c39ac440da7e6c2e774b400607d9eaa27ac707d7
AgustinCitti <103970902+AgustinCitti@users.noreply.github.com>
2022-08-28 22:05:21 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
-import './components/NavBar.css';
+import NavBar from './components/NavBar/NavBar.jsx';
+import ItemListContainer from './components/ItemListContainer.js';
 function App() {
   return (
     
@@ -12,7 +12,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         
         <h1>Cerveza Castelo</h1>
+        <ItemListContainer CervezasNombre='Kolsch' CervezaDescripcion='Es una cerveza clara, su tonalidad es amarilla brillante y tiene un gusto prominente, pero no extremo de lúpulo. '/>
       </header>
+      
     </div>
   );
 }
